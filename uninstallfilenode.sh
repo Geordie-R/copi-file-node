38725fb0d28583a9fe1db4c340e0485b3592ece8
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2025-02-19 21:57:56 +0000

    Update uninstallfilenode.sh

--- uninstallfilenode.sh
+++ uninstallfilenode.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 #This will uninstall the filenode so that you can run the install again.
-# Note this will do the following:
+# Note this will remove the directory at /home/<yourusername>/filenode
+
+read -p "What is your ubuntu username (its likely to be copi if you followed the default suggestion on the install) ?: " username
+rm -R /home/$username/filenode 2> /dev/null
 
