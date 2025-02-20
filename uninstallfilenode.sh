d6abe5d820fc0a8bdc8488bf13bc1d1d02b3c127
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2025-02-20 01:20:58 +0000

    Update uninstallfilenode.sh

--- uninstallfilenode.sh
+++ uninstallfilenode.sh
@@ -6,7 +6,6 @@
 read -p "What is your ubuntu username (its likely to be copi if you followed the default suggestion on the install) ?: " username
 echo "Removing docker image that contains pool-server..."
 docker images | grep "pool-server" | awk '{print $3}' | xargs docker rmi -f
-
 echo "Removing /home/$username/filenode"
 rm -R /home/$username/filenode 2> /dev/null
 
