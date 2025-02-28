4518ed169ef94ae747b0850ebef82599fcc5638e
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2025-02-28 01:00:02 +0000

    Update uninstallfilenode.sh

--- uninstallfilenode.sh
+++ uninstallfilenode.sh
@@ -3,22 +3,22 @@
 # This will uninstall the filenode so that you can run the install again.
 # Note this will remove the directory at /home/<yourusername>/filenode
 
-read -p "What is your ubuntu username (its likely to be copi if you followed the default suggestion on the install) ?: " username
+read -p "What is your ubuntu username (it's likely to be copi or root) ?: " username
+
+read -p "What is your container name? (it's likely to be COPNode1 if you accepted defaults on the install) ?: " ContainerName
+
+
 echo "Removing docker image that contains pool-server..."
 # Get Container ID
-container_id=$(docker ps | grep "pool-server" | awk '{print $1}')
 
-if [ -n "$container_id" ]; then
-    echo "Stopping container: $container_id"
-    docker stop "$container_id"
-    
-    echo "Removing container: $container_id"
-    docker rm "$container_id"
+# Check if the container exists
+if docker ps -a --format '{{.Names}}' | grep -w "$ContainerName" &> /dev/null; then
+    echo "Container $ContainerName found. Removing it..."
+    docker rm -f "$ContainerName"
 else
-    echo "No running container found for pool-server."
+    echo "Container $ContainerName not found."
 fi
-
 
 echo "Removing /home/$username/filenode"
 rm -R /home/$username/filenode 2> /dev/null
-
+echo "Script Complete"
