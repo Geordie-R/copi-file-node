e25ed2daa7e1f31f6901b52ecbe46c49537474d5
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2025-02-28 01:07:52 +0000

    Update uninstallfilenode.sh

--- uninstallfilenode.sh
+++ uninstallfilenode.sh
@@ -3,7 +3,14 @@
 # This will uninstall the filenode so that you can run the install again.
 # Note this will remove the directory at /home/<yourusername>/filenode
 
-read -p "What is your ubuntu username (it's likely to be copi or root) ?: " username
+read -p "What is your ubuntu username (Leave it empty to just use $USER) ?: " username
+
+if [[ $username == "" ]] || [ -z "$username" ];
+then
+  username=$USER
+fi
+
+user_home=$(eval echo "~$username")
 
 read -p "What is your container name? (it's likely to be COPNode1 if you accepted defaults on the install) ?: " ContainerName
 
@@ -19,6 +26,6 @@ else
     echo "Container $ContainerName not found."
 fi
 
-echo "Removing /home/$username/filenode"
-rm -R /home/$username/filenode 2> /dev/null
+echo "Removing $user_home/filenode"
+rm -R $user_home/filenode 2> /dev/null
 echo "Script Complete"
