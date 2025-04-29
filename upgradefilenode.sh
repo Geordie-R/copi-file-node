b4335523f9c986ab74f253c4f61fd6d5c7a905fd
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2025-04-30 00:29:10 +0100

    Update upgradefilenode.sh

--- upgradefilenode.sh
+++ upgradefilenode.sh
@@ -150,6 +150,36 @@ user_home=$(eval echo "~$username")
 cacheurl="$user_home/$node_folder/cache"
 cd $user_home/$node_folder/
 
+get_yaml_value() {
+    local key="$1"
+    local file="docker-compose.yml"
+
+    # Use grep to find the line, then cut to get the value after the colon
+    local value=$(grep -E "^[[:space:]]*$key:" "$file" | sed -E "s/^[[:space:]]*$key:[[:space:]]*//")
+
+    echo "$value"
+}
+
+
+PoolPortNo=$(get_yaml_value "FILENODES_POOL_PUBLIC_PORT")
+
+
+if [[ -n "$PoolPortNo" ]]; then
+echo "${GREEN}Found your pool port number in the docker-compose.yml file: $PoolPortNo ${COLOR_RESET}"
+else
+echo "${RED}Did not find your pool port number in the docker-compose.yml file: $PoolPortNo ${COLOR_RESET}"
+fi
+
+
+
+if [[ -n "$PoolAccessKey" && $action == "filenode" ]]; then
+     # Replace the line while preserving indentation
+    sed -i -E "s/^([[:space:]]*FILENODES_POOL_ACCESS_KEY:).*/\1 $PoolAccessKey/" docker-compose.yml
+    echo "${GREEN}Pool Access Key Updated in docker-compose.yml ${COLOR_RESET}"
+else
+    echo "Did not update pool access key as no key was given or node type is not filenode"
+fi
+
 
 serverurl=http://$serverip:$PoolPortNo
 healthurl=$serverurl/health
@@ -183,35 +213,7 @@ fi
 
 logging_file_name="";
 
-get_yaml_value() {
-    local key="$1"
-    local file="docker-compose.yml"
 
-    # Use grep to find the line, then cut to get the value after the colon
-    local value=$(grep -E "^[[:space:]]*$key:" "$file" | sed -E "s/^[[:space:]]*$key:[[:space:]]*//")
-
-    echo "$value"
-}
-
-
-PoolPortNo=$(get_yaml_value "FILENODES_POOL_PUBLIC_PORT")
-
-
-if [[ -n "$PoolPortNo" ]]; then
-echo "${GREEN}Found your pool port number in the docker-compose.yml file: $PoolPortNo ${COLOR_RESET}"
-else
-echo "${RED}Did not find your pool port number in the docker-compose.yml file: $PoolPortNo ${COLOR_RESET}"
-fi
-
-
-
-if [[ -n "$PoolAccessKey" && $action == "filenode" ]]; then
-     # Replace the line while preserving indentation
-    sed -i -E "s/^([[:space:]]*FILENODES_POOL_ACCESS_KEY:).*/\1 $PoolAccessKey/" docker-compose.yml
-    echo "${GREEN}Pool Access Key Updated in docker-compose.yml ${COLOR_RESET}"
-else
-    echo "Did not update pool access key as no key was given or node type is not filenode"
-fi
 
 read -n 1 -r -s -p $'Press enter to do a docker compose pull -d...\n'
 
